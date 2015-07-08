612bc8d92f5efaac77a378214e11d4ba8528c9aa
Stanislav Petryakov <become.glory@yandex.ru>
2015-07-08 13:35:03 +0300

    Add ListView to MainActivity

--- app/src/main/java/com/nextgis/rehacompdemo/MainActivity.java
+++ app/src/main/java/com/nextgis/rehacompdemo/MainActivity.java
@@ -1,38 +1,26 @@
 package com.nextgis.rehacompdemo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        ListView lvAddresses = (ListView)findViewById(R.id.lv_addresses);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        final String[] addresses = new String[] {
+                getString(R.string.address1),
+                getString(R.string.address2)
+        };
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, addresses);
+        lvAddresses.setAdapter(adapter);
     }
 }
